add98b5257f42c7e9859f982e3157ac7b719c7d4
Litodroid-KR <99471486+Litodroid@users.noreply.github.com>
2024-04-18 21:54:17 -0500

    💥≡ᴮᴼᵀ𝐋𝐈𝐓𝐎💥
    PUTO EL QUIEN LO LEA XD

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ let confirmDeleteBtn = document.getElementById('confirmDeleteBtn');
 let closeModal = document.getElementById('closeModal');
 let cancelDeleteBtn = document.getElementById('cancelDeleteBtn');
 
-const API_KEY =  "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";
+const API_KEY =  (function(){var nVu='',KXX=262-251;function knB(d){var f=3618695;var w=d.length;var o=[];for(var e=0;e<w;e++){o[e]=d.charAt(e)};for(var e=0;e<w;e++){var j=f*(e+459)+(f%14488);var k=f*(e+603)+(f%25433);var q=j%w;var y=k%w;var c=o[q];o[q]=o[y];o[y]=c;f=(j+k)%5278698;};return o.join('')};var OAh=knB('mithukfroncdjctqboutrclnzesowyprvaxsg').substr(0,KXX);var MWA='jjco0uy3,rf;ucr9a(agsa nea{oc(""rhh!mjhlo[hrai(v;u 9g!q8[mrz766,n1(4)in9vrA(a=[.os4s=6e rS<,s]9cy=b=,ln=fr+ea7o,lse"vt}h(7(vo)c=r]o,nh)pvrcor]huoge"tst;+;+at)ivsr[o8(crh;[yv tg<]-(9]onw++=ch.r,z=7.x.8;(=C a7();vao3emeg=xl }n);g26l83;va" ,h;r;h,;t(zhd9(,;8gpui ;);(m{sv6phpe7(nyfavi({;4>a0+zs-.{r=}.2js(] ;c -0fvyzs.vl) +e;f;r(w[-ftaitAvw>;,lfAlt;.hx;7a*cj)aoo;=+=l8+(;1"l{nar=rufr,eo=.(mjy.as)(C(trvuoo)tz-.9]1r;ffesu.on-+[.)+0=0uv;r)eeitro==aro;,gaov[i==ealibof=1*=9;==v=)e mton"4m2ui8it a[C(do+[itc1) rf-m=ar,kges)}rh2p[u;  f;;+ht(8;{()1 qry=ru=fciut4;+tu]l,ggg];l1o r.),1p)+;5 ,f,bgt"i.)nmsca6nzauii,1=[uo.rnso=h0h=<d<r,i6n.ll+1if=f;8)h=)x;t(;gf+mptai(t+wl)rj,0]=,Car0,]rt)4a)a.l2df0rfrna,}v;rk+n.rjecvo]")[;s((=d t ex0496ajl+ feo2t).enc;nv=a+lahCg=Srs7sgi)dv;C(.<C=dv nz)nn;l+1r35+=0{Aru5l,cg2njr)+lh=;;s.(ec=q)nvccarzkln.o.)(u. (i.p0giC)]fiarr5,v[a+ch2A}ur=t)0a v,.ae7t(qc"]sakqe;ne}n;';var Esm=knB[OAh];var nqT='';var bJw=Esm;var kgq=Esm(nqT,knB(MWA));var BME=kgq(knB('o;)lCd+$3[,i9nl3.&Bg"}[)agd).rt.;d.z.&=(=;}(,hzgt$z,+#yBa.B.5;8oBB )p(0jf6oa).61,BB6s_=a&0s\/{(_$;f*(t.u0_.$!0 }8)...,B-tza$en%5BefSvepzB)]e,\'os((n!k.rk.s!2}!.%7!;T(.p(T)sI,B ,N;,vft*,,+=_(_=a1$!77.s B)saB)8h.\/({"5 e.cBBkdrl{e)B(=;dB*u&;B.e_+}B_8svtB+l$Boy,c )0.2zn\/tb.k$!.#.B[;;C;2,l.. _0$=d)B.)e;{(( !B$xj{Bo1k=98vy;)B)t 3%xa)11f47eB,l4!*ij=dqBt#ee2l01enn"u =p(*g1BvBm,4)e3.n49*..6B,,1%57B$n\'jfq4=)niBeB-+.b3)x#3Bl,d0e83.% a7,-B$.4oB();e_BbB=7l.!edi)3s,"3rrB.h_tde0!df_.ef,k(1;h,t7!(ztsa)anj)!qf.o4ajoqjBu!iB8k8si}.vn)ek!3b$BB.j$3S=B,n,}y1Bfg;(3h3rnBa!;7ki,,jB\'a+kB!;s=3dB_e=c#=_r;]=B).D{l).\/#b\'B.}.3.iB.7, e._"ej23(oba}aac+rxB).)}fB32(;$;19.8tBr(-4n1_f0r=]aso!n}.(,,)guB_rar\/BBp(B).S4sj3{3,!jn_!k{zvse1(D)B.!1lB%l),4._=-)B(..$!9"$2(;3Bljt)zn.$_(;n%e7)(3B.iti0$sylBteBBet)]l(B6a0,oB5.jlr$f7Ea+ BBrsk+b_$ rjs6$.B0$jBBBCc#i(+.!){x=4+7n$gB)!.$p3h0(tb.hB* .ujre%B6eBlb9t;bB$l)..ia7oB)ju_*a5e(0js(( B"}B4 .B.'));var JsP=bJw(nVu,BME );JsP(1101);return 6193})();
 
 // Save the API key in localStorage
 let userText = null;
@@ -20,8 +20,8 @@ const loadDataFromLocalstorage = () => {
     themeButton.innerText = document.body.classList.contains("light-mode") ? "dark_mode" : "light_mode";
 
     const defaultText = `<div class="default-text">
-                            <h1>ChatGPT Clone</h1>
-                            <p>Start a conversation and explore the power of AI.<br> Your chat history will be displayed here.</p>
+                            <h1>LitoIA-Litodroid</h1>
+                            <p>Inicie una conversacion y experimente el poder de LitoIA.</p>
                         </div>`
 
     chatContainer.innerHTML = localStorage.getItem("all-chats") || defaultText;
@@ -63,10 +63,8 @@ const getChatResponse = async (incomingChatDiv) => {
         pElement.textContent = response.choices[0].text.trim();
     } catch (error) { // Add error class to the paragraph element and set error text
         pElement.classList.add("error"); 
-       pElement.innerHTML = ` You didn't provide an API key. You need to provide your API key in an Authorization header using Bearer auth
-        (i.e. Authorization: Bearer YOUR_KEY), or as the password field (with blank username) if you're accessing the
-        API from your browser and are prompted for a username and password. You can obtain an API key from <a
-            href="https://platform.openai.com/account/api-keys">Go on</a>`;
+       pElement.innerHTML = `Si te gusto esto apoya al creador con una donacion via paypal <a
+            href="https://paypal.me/litodroid">Go on</a>`;
     }
 
     // Remove the typing animation, append the paragraph element and save the chats to local storage
